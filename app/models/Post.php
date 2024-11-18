e573134736c6bd2c674e1c6fe04b71bb09590197
Dennis F <123222958+dennisfrenkel@users.noreply.github.com>
2024-11-17 23:09:10 -0500

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -10,23 +10,24 @@ class Post extends Model {
         return $this->fetchAll($query);
     }
 
-    public function getPostById($id) {
+    public function getPostById($id){
         $query = "SELECT * FROM posts WHERE id = :id";
-        return $this->fetch($query, ['id' => $id]);
+        return $this->fetchWithParams($query, ['id' => $id]);
     }
 
-    public function savePost($inputData) {
+    public function savePost($inputData){
         $query = "INSERT INTO posts (title, content) VALUES (:title, :content)";
-        return $this->fetchAllWithParams($query, $inputData);
+        return $this->executeWithParams($query, $inputData);
     }
 
-    public function updatePost($inputData) {
+    public function updatePost($inputData, $id){
         $query = "UPDATE posts SET title = :title, content = :content WHERE id = :id";
-        return $this->fetchAllWithParams($query, $inputData);
+        $inputData['id'] = $id;
+        return $this->executeWithParams($query, $inputData);
     }
 
-    public function deletePost($inputData) {
+    public function deletePost($id){
         $query = "DELETE FROM posts WHERE id = :id";
-        return $this->fetchAllWithParams($query, $inputData);
+        return $this->executeWithParams($query, ['id' => $id]);
     }
 }
