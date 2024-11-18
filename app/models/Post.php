1fbb5c1ba81ea5a0b9484d87984b150b88694622
Dennis F <123222958+dennisfrenkel@users.noreply.github.com>
2024-11-18 15:32:53 -0500

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,30 +4,34 @@ namespace app\models;
 use app\models\Model;
 
 class Post extends Model {
+    public function getAllPostsByTitle($title) {
+        $query = "SELECT * FROM posts WHERE title LIKE :title";
+        return $this->fetchAllWithParams($query, ['title' => '%' . $title . '%']);
+    }
 
     public function getAllPosts() {
         $query = "SELECT * FROM posts";
         return $this->fetchAll($query);
     }
 
-    public function getPostById($id){
+    public function getPostById($id) {
         $query = "SELECT * FROM posts WHERE id = :id";
-        return $this->fetchWithParams($query, ['id' => $id]);
-    }
+        $result = $this->fetchOneWithParams($query, ['id' => $id]); // Use fetchOne for a single post
+        return $result ?: null; 
 
-    public function savePost($inputData){
+
+    public function savePost($inputData) {
         $query = "INSERT INTO posts (title, content) VALUES (:title, :content)";
-        return $this->executeWithParams($query, $inputData);
+        return $this->executeWithParams($query, $inputData); // Assuming executeWithParams is available in the parent Model class
     }
 
-    public function updatePost($inputData, $id){
+    public function updatePost($inputData) {
         $query = "UPDATE posts SET title = :title, content = :content WHERE id = :id";
-        $inputData['id'] = $id;
-        return $this->executeWithParams($query, $inputData);
+        return $this->executeWithParams($query, $inputData); // Assuming executeWithParams is available in the parent Model class
     }
 
-    public function deletePost($id){
+    public function deletePost($inputData) {
         $query = "DELETE FROM posts WHERE id = :id";
-        return $this->executeWithParams($query, ['id' => $id]);
+        return $this->executeWithParams($query, $inputData); // Assuming executeWithParams is available in the parent Model class
     }
 }
